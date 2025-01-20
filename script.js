45711de39a155872ac05707ce0117f9f4c6d013f
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-01-20 11:09:42 +0530

    Scope & Lexical Environment

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hello")
 // alert("Hello")
+
+// Scope & Lexical Environment
+
+function a(){
+    var b = 10;
+    c();
+    function c(){
+        console.log(b);
+    }
+}
+
+a();
+// console.log(b);
