c0fc41f693fad3c412f5842708c0a10b4d87de0a
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-01-21 11:41:58 +0530

    Date and Time

--- script.js
+++ script.js
@@ -98,3 +98,37 @@ const min = 10;
 const max = 20;
 
 console.log(Math.floor(Math.random()*(max-min+1)));
+
+// Date and Time
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23);
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2023,0,23,5,3);
+let myCreatedDate = new Date("2023-01-14");
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default',{
+    weekday: "long",
+    // timeZone
+}));
+
+// Arrays in JavaScript
+
